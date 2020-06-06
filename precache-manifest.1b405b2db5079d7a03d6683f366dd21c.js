a934f5ebd89e706d12406f911ac1d4a97aafa8cb
Daniel Duran Jr <danielduranjr@Daniels-Air.attlocal.net>
2020-06-06 12:13:12 -0500

    Updates

---
+++ precache-manifest.1b405b2db5079d7a03d6683f366dd21c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44d4f691fc80bca6264d818804e6fee",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "0ae61d8edb9b4289f453",
+    "url": "/ReactPortfolio/static/css/main.85f7a415.chunk.css"
+  },
+  {
+    "revision": "879110023386a5de50a3",
+    "url": "/ReactPortfolio/static/js/2.5629b473.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.5629b473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ae61d8edb9b4289f453",
+    "url": "/ReactPortfolio/static/js/main.ccc2cc64.chunk.js"
+  },
+  {
+    "revision": "d737db38bfc28d508ea3",
+    "url": "/ReactPortfolio/static/js/runtime-main.0b506dc4.js"
+  },
+  {
+    "revision": "0b54d1142df07a212a443f0d5bfd5701",
+    "url": "/ReactPortfolio/static/media/careerResourcesWiki.0b54d114.jpg"
+  },
+  {
+    "revision": "60703539ac5b107b191ebf20333cda85",
+    "url": "/ReactPortfolio/static/media/danielDuranResume.60703539.pdf"
+  },
+  {
+    "revision": "f570e9a79dd4d5bd41450f3a23f9b218",
+    "url": "/ReactPortfolio/static/media/danielHeadshot.f570e9a7.jpg"
+  },
+  {
+    "revision": "9db691d49b837da91041d727059c9a33",
+    "url": "/ReactPortfolio/static/media/eTracker.9db691d4.gif"
+  },
+  {
+    "revision": "bb1a5ecc47c9d633f25525878dc0e7d6",
+    "url": "/ReactPortfolio/static/media/passwordGen.bb1a5ecc.png"
+  },
+  {
+    "revision": "326fe223f1c990b166d81af5d04bef67",
+    "url": "/ReactPortfolio/static/media/sOUNDthis.326fe223.png"
+  },
+  {
+    "revision": "6a5489e723f064f892ba5d9dc0b24b57",
+    "url": "/ReactPortfolio/static/media/teamGenerator.6a5489e7.png"
+  },
+  {
+    "revision": "51c2621a029de1d9789b00a929a69b8a",
+    "url": "/ReactPortfolio/static/media/weatherDash.51c2621a.png"
+  }
+]);
